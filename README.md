de0839c5ace01e86e3f60cb77ed34bd5c466eac0
Gvins <158508670+Gvinses@users.noreply.github.com>
2024-03-10 13:30:44 +0300

    register
    HTML, CSS, JavaScript registration part with converting Username to numbers, but it cann't save in database

--- README.md
+++ README.md
@@ -4,5 +4,6 @@ Easy website, which I use for leveling up my HTML, CSS and some JavaScript skill
  2. It is no adaptive website, because i can't create adaptive website yet.
  3. It is like a mockup to Online Shop, so no logic with SQL or Backend yet.
  4. I add a new part of page with something like price tag of product
+ 5. Registration part, working only for consol.log and converting name to numbers (no google sheets or  something like it yet)
 
-Version: 1.1 (Some updates soon)
+Version: 1.2 (Some updates soon)
--- script.js
+++ script.js
@@ -12,3 +12,36 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden,.hid')
 hiddenElements.forEach((el) => observer.observe(el));
+
+let username;
+let email;
+let id = Math.random();
+
+document.getElementById("register").onclick = function(){
+
+    username = document.getElementById("username").value;
+    console.log(username);
+
+    email = document.getElementById("email").value;
+    console.log(email);
+
+    if(document.getElementById("getTheaters").checked == true){
+        console.log("User agree")
+    }
+    else{
+        console.log("User not agree")
+    }
+    console.log(id)
+        // Преобразовать имя пользователя в массив букв
+        const letters = username.split("");
+        const letterEmail = email.split("");
+    
+        // Преобразовать каждую букву в число
+        const numbers = letters.map(letter => letter.charCodeAt() - 96);
+        const numberEmail = letterEmail.map(letterE => letterE.charCodeAt() - 12)
+    
+        // Вывести имя пользователя и преобразованные числа в консоль
+        console.log(`${numbers}`);
+        console.log(`${numberEmail}`);
+        // document.getElementById("output").innerHTML = "Your name = " + username + "Your e-mail = "+ email;
+}
